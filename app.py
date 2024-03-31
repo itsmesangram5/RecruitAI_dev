96da9c6ef2c2470b26ec28e7ac22e12aa78634a5
Sangram Nangare <itsmesangrm5@gmail.com>
2024-03-31 20:02:07 +0530

    Added view folder

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask
-
-app=Flask(__name__)
+from flask import Flask , render_template , request
+app=Flask(__name__,template_folder='view/templates')
 
 @app.route("/")
 def welcome():
-    return "Hello word Sangram"
+    return render_template('index.html')
 
 @app.route("/home")
 def home():
